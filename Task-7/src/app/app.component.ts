7270daf8fb104d243a778ecf25fba93a8a746385
bhargav <bhargav.cse2001@gmail.com>
2023-04-17 11:15:39 +0530

    task-7

---
+++ Task-7/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { datafields } from './appdata/appinfo';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  services:string[] = datafields.servicelist;
+  descriptions:string[] =
+  [ datafields.seo,
+  datafields.branding,
+datafields.logo
+]
+
+ dataitems=datafields;
+}
---
+++ Task-7/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceBoxComponent } from './service-box/service-box.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ServiceBoxComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
